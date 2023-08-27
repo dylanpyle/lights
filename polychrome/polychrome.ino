1711f8c26d63bf76acc1871b114f1ec8baa00f7a
Dylan Pyle <me@dylanpyle.com>
2023-08-26 20:48:23 -0400

    add burning man light setup

--- polychrome/polychrome.ino
+++ polychrome/polychrome.ino
@@ -1,51 +1,63 @@
 #include "LPD8806.h"
 #include "SPI.h"
 
-int nLEDs = 32;
+int nLEDs = 20;
 
-int dataPin  = 13;
+int dataPin = 13;
 int clockPin = 12;
+int maxColorValue = 127;
 
 LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+
+int currentProgram;
+int programCount = 6;
 
 void setup() {
   strip.begin();
   strip.show();
+
   Serial.begin(9600);
+
+  randomSeed(analogRead(0));
+  currentProgram = random(0, programCount - 1);
 }
 
-int rgb[3];
-int currentInt = 0;
-char incomingByte;
-
 void loop() {
-  if(Serial.available() > 0){
-    currentInt = 0;
-    while(1){
-      incomingByte = Serial.read();
-      
-      if(incomingByte == 'R') rgb[0] = currentInt;
-      if(incomingByte == 'G') rgb[1] = currentInt;
-      if(incomingByte == 'B') rgb[2] = currentInt;
-      if(incomingByte == 'R' || incomingByte == 'G' || incomingByte == 'B'){
-        currentInt = 0;
-        continue; 
-      }
-      if(incomingByte == -1) continue;
-      if(incomingByte == '\n') break;
-        
-      
-      currentInt *= 10;
-      currentInt = ((incomingByte - 48) + currentInt);
-      
+  Serial.println("running program:");
+  Serial.println(currentProgram);
+
+  if (currentProgram == 0) {
+    for (int i = 0; i < 5; i++) {
+      double r = random(0, maxColorValue);
+      double g = random(0, maxColorValue);
+      double b = random(0, maxColorValue);
+      colorChase(strip.Color(r, g, b), 50);
     }
-    colorWipe(strip.Color(rgb[0], rgb[1], rgb[2]), 20);
-    Serial.println("Wiping:");
-    Serial.println(rgb[0]);
-    Serial.println(rgb[1]);
-    Serial.println(rgb[2]);
-    
+  } else if (currentProgram == 1) {
+    for (int i = 0; i < 3; i++) {
+      rainbow(20);
+    }
+  } else if (currentProgram == 2) {
+    double r = random(0, maxColorValue);
+    double g = random(0, maxColorValue);
+    double b = random(0, maxColorValue);
+    colorWipe(strip.Color(r, g, b), 50);
+  } else if (currentProgram == 3) {
+    for (int i = 0; i < 3; i++) {
+      rainbowCycle(20);
+    }
+  } else if (currentProgram == 4) {
+    double r = random(0, maxColorValue);
+    double g = random(0, maxColorValue);
+    double b = random(0, maxColorValue);
+    colorWipe(strip.Color(r, g, b), 50);
+  } else if (currentProgram == 5) {
+    for (int i = 0; i < 3; i++) {
+      pixelHaze(10);
+    }
   }
+
+  currentProgram = (currentProgram + 1) % programCount;
 }
 
 /*
@@ -60,28 +72,28 @@ void loop() {
 
 void rainbow(uint8_t wait) {
   int i, j;
-   
-  for (j=0; j < 384; j++) {     // 3 cycles of all 384 colors in the wheel
-    for (i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel( (i + j) % 384));
+
+  for (j = 0; j < 384; j++) {  // 3 cycles of all 384 colors in the wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) % 384));
     }
-    strip.show();   // write all the pixels out
+    strip.show();  // write all the pixels out
     delay(wait);
   }
 }
 
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
-  
-  for (j=0; j < 384 * 5; j++) {     // 5 cycles of all 384 colors in the wheel
-    for (i=0; i < strip.numPixels(); i++) {
+
+  for (j = 0; j < 384 * 5; j++) {  // 5 cycles of all 384 colors in the wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       // tricky math! we use each pixel as a fraction of the full 384-color wheel
       // (thats the i / strip.numPixels() part)
       // Then add in j which makes the colors go around per pixel
       // the % 384 is to make the wheel cycle around
-      strip.setPixelColor(i, Wheel( ((i * 384 / strip.numPixels()) + j) % 384) );
-    }  
-    strip.show();   // write all the pixels out
+      strip.setPixelColor(i, Wheel(((i * 384 / strip.numPixels()) + j) % 384));
+    }
+    strip.show();  // write all the pixels out
     delay(wait);
   }
 }
@@ -90,24 +102,52 @@ void rainbowCycle(uint8_t wait) {
 void colorWipe(uint32_t c, uint8_t wait) {
   int i;
 
-  for (i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
+
+uint32_t hslToRgb(double h, double s, double l) {
+  double r, g, b;
+
+  if (s == 0) {
+    r = g = b = l;  // achromatic
+  } else {
+    double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    double p = 2 * l - q;
+    r = hueToRgb(p, q, h + 1 / 3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1 / 3);
+  }
+
+  return strip.Color(round(r * maxColorValue), round(g * maxColorValue), round(b * maxColorValue));
+}
+
+uint8_t hueToRgb(double p, double q, double t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
+}
+
 
 void pixelHaze(uint8_t wait) {
   int i;
 
-  for (i=0; i < 200; i++) {
-      int pin = random(0, strip.numPixels()-1);
-      int brightness = random(0,127);
-      uint32_t color = strip.Color(random(0, brightness), random(0,brightness), random(0,brightness));
-      
-      strip.setPixelColor(pin, color);
-      strip.show();
-      delay(wait);
+  for (i = 0; i < 200; i++) {
+    int pin = random(0, strip.numPixels());
+    double h = random(0, maxColorValue);
+    double s = random(4, maxColorValue);
+    double l = random(0, maxColorValue);
+    uint32_t color = hslToRgb(h, s, l);
+
+    strip.setPixelColor(pin, strip.Color(h, s, l));
+    strip.show();
+    delay(wait);
   }
 }
 
@@ -117,17 +157,17 @@ void colorChase(uint32_t c, uint8_t wait) {
   int i;
 
   // Start by turning all pixels off:
-  for(i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, 0);
+  for (i = 0; i < strip.numPixels(); i++) strip.setPixelColor(i, 0);
 
   // Then display one pixel at a time:
-  for(i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c); // Set new pixel 'on'
-    strip.show();              // Refresh LED states
-    strip.setPixelColor(i, 0); // Erase pixel, but don't refresh!
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);  // Set new pixel 'on'
+    strip.show();               // Refresh LED states
+    strip.setPixelColor(i, 0);  // Erase pixel, but don't refresh!
     delay(wait);
   }
 
-  strip.show(); // Refresh to turn off last pixel
+  strip.show();  // Refresh to turn off last pixel
 }
 
 /* Helper functions */
@@ -135,26 +175,24 @@ void colorChase(uint32_t c, uint8_t wait) {
 //Input a value 0 to 384 to get a color value.
 //The colours are a transition r - g -b - back to r
 
-uint32_t Wheel(uint16_t WheelPos)
-{
+uint32_t Wheel(uint16_t WheelPos) {
   byte r, g, b;
-  switch(WheelPos / 128)
-  {
+  switch (WheelPos / 128) {
     case 0:
-      r = 127 - WheelPos % 128;   //Red down
-      g = WheelPos % 128;      // Green up
-      b = 0;                  //blue off
-      break; 
+      r = maxColorValue - WheelPos % 128;  //Red down
+      g = WheelPos % 128;                  // Green up
+      b = 0;                               //blue off
+      break;
     case 1:
-      g = 127 - WheelPos % 128;  //green down
-      b = WheelPos % 128;      //blue up
-      r = 0;                  //red off
-      break; 
+      g = maxColorValue - WheelPos % 128;  //green down
+      b = WheelPos % 128;                  //blue up
+      r = 0;                               //red off
+      break;
     case 2:
-      b = 127 - WheelPos % 128;  //blue down 
-      r = WheelPos % 128;      //red up
-      g = 0;                  //green off
-      break; 
+      b = maxColorValue - WheelPos % 128;  //blue down
+      r = WheelPos % 128;                  //red up
+      g = 0;                               //green off
+      break;
   }
-  return(strip.Color(r,g,b));
+  return (strip.Color(r, g, b));
 }
